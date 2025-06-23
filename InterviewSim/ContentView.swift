0cb2d273dd497347f1000008f8c1535ebec4d3a1
Ammar Sufyan <63483228+ammarsufyan@users.noreply.github.com>
2025-06-23 20:50:30 +0700

    Initial Commit

---
+++ InterviewSim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InterviewSim
+//
+//  Created by Ammar Sufyan on 23/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InterviewSim/InterviewSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewSimApp.swift
+//  InterviewSim
+//
+//  Created by Ammar Sufyan on 23/06/25.
+//
+
+import SwiftUI
+
+@main
+struct InterviewSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
